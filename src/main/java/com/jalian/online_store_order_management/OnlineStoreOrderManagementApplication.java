52486044e3cef738733e81f901744b6d5c5f7c93
amirhosein jalian <amirhoseinjalian82@gmail.com>
2025-03-26 16:14:19 +0330

    initial commit

---
+++ src/main/java/com/jalian/online_store_order_management/OnlineStoreOrderManagementApplication.java
@@ -0,0 +1,13 @@
+package com.jalian.online_store_order_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreOrderManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreOrderManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jalian/online_store_order_management/OnlineStoreOrderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jalian.online_store_order_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreOrderManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
