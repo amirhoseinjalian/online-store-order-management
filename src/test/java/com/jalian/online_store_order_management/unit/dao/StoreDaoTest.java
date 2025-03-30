b273ac98bfb82fc85a84271b6064a6c3725bbc67
amirhosein jalian <amirhoseinjalian82@gmail.com>
2025-03-31 00:22:47 +0330

    test: necessary dao test units added

---
+++ src/test/java/com/jalian/online_store_order_management/unit/dao/StoreDaoTest.java
@@ -0,0 +1,96 @@
+package com.jalian.online_store_order_management.unit.dao;
+
+import com.jalian.online_store_order_management.dao.StoreDao;
+import com.jalian.online_store_order_management.domain.Store;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class StoreDaoTest extends BaseDomainRepositoryTest<Store, StoreDao> {
+
+    public StoreDaoTest(@Autowired StoreDao repository) {
+        super(repository);
+    }
+
+    @Override
+    @BeforeEach
+    public void initializeValue() {
+        instanceToTest = new Store("Test Store " + UUID.randomUUID());
+        instanceToTest.setUsers(new ArrayList<>());
+    }
+
+    @Test
+    public void testFindByIdSafe_whenStoreExists_returnsStore() {
+        Store store = new Store("Store A");
+        store.setUsers(new ArrayList<>());
+        Store savedStore = repository.save(store);
+
+        var foundStore = repository.findByIdSafe(savedStore.getId());
+
+        assertThat(foundStore).isPresent();
+        assertThat(foundStore.get().getId()).isEqualTo(savedStore.getId());
+        assertThat(foundStore.get().getName()).isEqualTo("Store A");
+    }
+
+    @Test
+    public void testFindByIdSafe_whenStoreDoesNotExist_returnsEmptyOptional() {
+        var foundStore = repository.findByIdSafe(999L);
+        assertThat(foundStore).isNotPresent();
+    }
+
+    @Test
+    public void testFindByNameSafe_whenStoreExists_returnsStore() {
+        var store = new Store("Unique Store");
+        store.setUsers(new ArrayList<>());
+        Store savedStore = repository.save(store);
+
+        var foundStore = repository.findByNameSafe("Unique Store");
+
+        assertThat(foundStore).isPresent();
+        assertThat(foundStore.get().getId()).isEqualTo(savedStore.getId());
+        assertThat(foundStore.get().getName()).isEqualTo("Unique Store");
+    }
+
+    @Test
+    public void testFindByNameSafe_whenStoreDoesNotExist_returnsEmptyOptional() {
+        var foundStore = repository.findByNameSafe("NonExistent Store");
+        assertThat(foundStore).isNotPresent();
+    }
+
+    @Override
+    protected Store updateValue() {
+        instanceToTest.setName("Updated Test Store");
+        return instanceToTest;
+    }
+
+    @Override
+    protected void updateMethodValidation(Store updatedInstance) {
+        assertThat(updatedInstance).isNotNull();
+        assertThat(updatedInstance.getName()).isEqualTo("Updated Test Store");
+    }
+
+    @Override
+    protected List<Store> findAllEntities() {
+        var store1 = new Store("Store 1");
+        store1.setUsers(new ArrayList<>());
+
+        var store2 = new Store("Store 2");
+        store2.setUsers(new ArrayList<>());
+
+        var store3 = new Store("Store 3");
+        store3.setUsers(new ArrayList<>());
+
+        return List.of(store1, store2, store3);
+    }
+}
---
+++ src/test/java/com/jalian/online_store_order_management/unit/dao/UserDaoTest.java
@@ -0,0 +1,95 @@
+package com.jalian.online_store_order_management.unit.dao;
+
+import com.jalian.online_store_order_management.dao.UserDao;
+import com.jalian.online_store_order_management.domain.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UserDaoTest extends BaseDomainRepositoryTest<User, UserDao> {
+
+    public UserDaoTest(@Autowired UserDao repository) {
+        super(repository);
+    }
+
+    @Override
+    @BeforeEach
+    public void initializeValue() {
+        instanceToTest = new User("testuser", "password");
+    }
+
+    @Test
+    public void testFindByUsername_whenUserExists_returnsUser() {
+        var user = new User("user1", "password");
+        var savedUser = repository.save(user);
+
+        var foundUser = repository.findByUsername("user1");
+
+        assertThat(foundUser).isPresent();
+        assertThat(foundUser.get().getId()).isEqualTo(savedUser.getId());
+        assertThat(foundUser.get().getUsername()).isEqualTo("user1");
+    }
+
+    @Test
+    public void testFindByUsername_whenUserDoesNotExist_returnsEmptyOptional() {
+        var foundUser = repository.findByUsername("nonexistent");
+        assertThat(foundUser).isNotPresent();
+    }
+
+    @Test
+    public void testFindUserById_whenUserExists_returnsUser() {
+        var user = new User("user2", "password");
+        var savedUser = repository.save(user);
+
+        var foundUser = repository.findUserById(savedUser.getId());
+
+        assertThat(foundUser).isPresent();
+        assertThat(foundUser.get().getId()).isEqualTo(savedUser.getId());
+    }
+
+    @Test
+    public void testFindUserById_whenUserDoesNotExist_returnsEmptyOptional() {
+        var foundUser = repository.findUserById(999L);
+        assertThat(foundUser).isNotPresent();
+    }
+
+    @Test
+    public void testFindUserByIdForUpdate_whenUserExists_returnsUserForUpdate() {
+        var user = new User("user3", "password");
+        var savedUser = repository.save(user);
+
+        var foundUser = repository.findUserByIdForUpdate(savedUser.getId());
+
+        assertThat(foundUser).isPresent();
+        assertThat(foundUser.get().getId()).isEqualTo(savedUser.getId());
+    }
+
+    @Override
+    protected User updateValue() {
+        instanceToTest.setUsername("updatedUser");
+        return instanceToTest;
+    }
+
+    @Override
+    protected void updateMethodValidation(User updatedInstance) {
+        assertThat(updatedInstance).isNotNull();
+        assertThat(updatedInstance.getUsername()).isEqualTo("updatedUser");
+    }
+
+    @Override
+    protected List<User> findAllEntities() {
+        var user1 = new User("user4", "password");
+        var user2 = new User("user5", "password");
+        var user3 = new User("user6", "password");
+
+        return List.of(user1, user2, user3);
+    }
+}
