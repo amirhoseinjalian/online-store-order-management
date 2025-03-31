eb626900baa74e8a156bbd28177635809da6f24d
amirhosein jalian <amirhoseinjalian82@gmail.com>
2025-04-01 00:01:10 +0330

    feat: findAllByOrder added to ItemDao

--- src/main/java/com/jalian/online_store_order_management/dao/ItemDao.java
+++ src/main/java/com/jalian/online_store_order_management/dao/ItemDao.java
@@ -3,8 +3,15 @@ package com.jalian.online_store_order_management.dao;
 import com.jalian.online_store_order_management.domain.Item;
 import com.jalian.online_store_order_management.domain.key.ItemKey;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ItemDao extends JpaRepository<Item, ItemKey> {
+
+    @Query("select i from Item i where i.order.id = :id")
+    List<Item> findAllByOrder(@Param("id") Long id);
 }
